9e265aaf3f94f156e087bdab40fb8146c938113a
Derek Leach <derekleach84@gmail.com>
2018-05-18 16:37:37 +0100

    seventh test

--- start_point/specs/pet_shop_spec.rb
+++ start_point/specs/pet_shop_spec.rb
@@ -110,7 +110,7 @@ class TestPetShop < Minitest::Test
     assert_equal(6, count)
   end
 
-  def test_all_pets_by_breed__found
+  def test_all_pets_by_breed_found
     pets = pets_by_breed(@pet_shop, "British Shorthair")
     assert_equal(2, pets.count)
   end
